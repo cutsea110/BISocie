b4518316a2ff0cd68270a253e25eb77b896be8c9
Katsutoshi Itoh <cutsea110@gmail.com>
2011-02-19 00:50:49 +0900

    modify proccess to create new project.

--- Handler/Project.hs
+++ Handler/Project.hs
@@ -4,6 +4,7 @@ module Handler.Project where
 
 import BISocie
 import Control.Monad (unless, forM, mplus)
+import Control.Applicative ((<$>),(<*>))
 import Data.Time
 
 import qualified Settings
@@ -16,32 +17,46 @@ getNewProjectR = do
       viewablehumannet = userRole self >= Teacher
   unless cancreateproject $ 
     permissionDenied "あなたはプロジェクトを作成することはできません."
+  let inintstatuses = "!未開始#赤\n着手#緑\n完了#灰\n=却下#灰\n保留\n議論\n報告"::String
+      help = $(Settings.hamletFile "help")
   defaultLayout $ do
     setTitle $ string "プロジェクト新規作成"
     addCassius $(cassiusFile "project")
+    addJulius $(juliusFile "help")
     addHamlet $(hamletFile "newproject")
     
 postNewProjectR :: Handler RepHtml
 postNewProjectR = do
-  (selfid, self) <- requireAuth
-  let cancreateproject = userRole self >= Teacher
-  unless cancreateproject $ 
-    permissionDenied "あなたはプロジェクトを作成することはできません."
-  now <- liftIO getCurrentTime
-  runDB $ do
-    pid <- insert $ Project { projectName=""
-                            , projectIssuecounter=0
-                            , projectDescription=Nothing
-                            , projectStatuses="!未開始#赤\n着手#緑\n完了#灰\n=却下#灰\n保留\n議論\n報告"
-                            , projectCuser=selfid
-                            , projectCdate=now
-                            , projectUdate=now
-                            }
-    _ <- insert $ Participants { participantsProject=pid 
-                               , participantsUser=selfid 
-                               , participantsReceivemail=True
-                               }
-    lift $ redirect RedirectTemporary $ ProjectR pid
+  _method <- lookupPostParam "_method"
+  case _method of
+    Just "create" -> createProject
+    _             -> invalidArgs ["The possible values of '_method' is create"]
+  where
+    createProject :: Handler RepHtml
+    createProject = do
+      (selfid, self) <- requireAuth
+      let cancreateproject = userRole self >= Teacher
+      unless cancreateproject $ 
+        permissionDenied "あなたはプロジェクトを作成することはできません."
+      (name, desc, sts) <- runFormPost'$ (,,)
+                           <$> stringInput "name"
+                           <*> stringInput "description"
+                           <*> stringInput "statuses"
+      now <- liftIO getCurrentTime
+      runDB $ do
+        pid <- insert $ Project { projectName=name
+                                , projectIssuecounter=0
+                                , projectDescription=desc
+                                , projectStatuses=sts
+                                , projectCuser=selfid
+                                , projectCdate=now
+                                , projectUdate=now
+                                }
+        _ <- insert $ Participants { participantsProject=pid 
+                                   , participantsUser=selfid 
+                                   , participantsReceivemail=True
+                                   }
+        lift $ redirect RedirectTemporary $ ProjectR pid
 
 getProjectR :: ProjectId -> Handler RepHtml
 getProjectR pid = do
@@ -56,9 +71,11 @@ getProjectR pid = do
     now <- liftIO getCurrentTime
     let (y,_,_) = toGregorian $ utctDay now
         eyears = [Settings.entryStartYear..y+5]
+        help = $(Settings.hamletFile "help")
     lift $ defaultLayout $ do
       setTitle $ string $ projectName prj
       addCassius $(cassiusFile "project")
+      addJulius $(juliusFile "help")
       addJulius $(juliusFile "project")
       addHamlet $(hamletFile "project")
 
@@ -82,8 +99,8 @@ putProjectR pid = do
     prj <- get404 pid
     Just nm <- (lift $ lookupPostParam "name") >>=
                \nm' -> return $ nm' `mplus` (Just $ projectName prj)
-    ds <- (lift $ lookupPostParam "description") >>=
-          \ds' -> return $ ds' `mplus` projectDescription prj
+    Just ds <- (lift $ lookupPostParam "description") >>=
+          \ds' -> return $ ds' `mplus` (Just $ projectDescription prj)
     Just st <- (lift $ lookupPostParam "statuses") >>=
                \st' -> return $ st' `mplus` (Just $ projectStatuses prj)
     now <- liftIO getCurrentTime
@@ -95,7 +112,7 @@ putProjectR pid = do
     lift $ do
       cacheSeconds 10 -- FIXME
       jsonToRepJson $ jsonMap [ ("name", jsonScalar $ projectName prj)
-                              , ("description", showMaybeJScalar $ projectDescription prj)
+                              , ("description", jsonScalar $ projectDescription prj)
                               , ("statuses", jsonScalar $ projectStatuses prj)
                               ]
   where

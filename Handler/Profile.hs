7bc1db6fecde3ae5eaa0e49189d01523e9ae9ef8
cutsea110 <cutsea110@research.timedia.co.jp>
2011-02-23 11:55:22 +0900

    FIX BUG: enbug which couldn't update non Student profile.

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -116,88 +116,103 @@ postProfileR uid = do
 putProfileR :: UserId -> Handler RepHtml
 putProfileR uid = do
   (selfid, self) <- requireAuth
-  (bir, ey, gy, br, zip, adr, lon, lat, tel, st, hzip, hadr, hlon, hlat, htel, dc, dwl, emp) <- 
-    runFormPost' $ (,,,,,,,,,,,,,,,,,)
-    <$> dayInput "birth"
-    <*> intInput "entryYear"
-    <*> maybeIntInput "graduateYear"
-    <*> stringInput "branch"
-    <*> stringInput "zip"
-    <*> stringInput "address"
-    <*> maybeStringInput "longitude"
-    <*> maybeStringInput "latitude"
-    <*> stringInput "tel"
-    <*> stringInput "station"
-    <*> stringInput "homeZip"
-    <*> stringInput "homeAddress"
-    <*> maybeStringInput "homeLongitude"
-    <*> maybeStringInput "homeLatitude"
-    <*> stringInput "homeTel"
-    <*> maybeStringInput "desiredCourse"
-    <*> maybeStringInput "desiredWorkLocation"
-    <*> maybeStringInput "employment"
-  let lon' = fromMaybe Nothing (fmap (Just . read) lon)
-      lat' = fromMaybe Nothing (fmap (Just . read) lat)
-      hlon' = fromMaybe Nothing (fmap (Just . read) hlon)
-      hlat' = fromMaybe Nothing (fmap (Just . read) hlat)
-  (em, fn, gn) <- 
-    runFormPost' $ (,,)
-    <$> emailInput "email"
-    <*> stringInput "familyName"
-    <*> stringInput "givenName"
-  runDB $ do
-    -- validate
-    user <- get404 uid
-    let editable = selfid == uid || userRole self > userRole user
-    unless editable $ 
-      lift $ permissionDenied "あなたはこのユーザプロファイルを編集することはできません."
-      -- update user
-    update uid [UserEmail em, UserFamilyName fn, UserGivenName gn]
-    mprof <- getBy $ UniqueProfile uid
-    case mprof of
-      Nothing -> do
-        insert $ Profile { profileUser=uid
-                         , profileBirth=bir
-                         , profileEntryYear=ey
-                         , profileGraduateYear=gy
-                         , profileBranch=br
-                         , profileZip=zip
-                         , profileAddress=adr
-                         , profileLongitude=lon'
-                         , profileLatitude=lat'
-                         , profileTel=tel
-                         , profileStation=st
-                         , profileHomeZip=hzip
-                         , profileHomeAddress=hadr
-                         , profileHomeLongitude=hlon'
-                         , profileHomeLatitude=hlat'
-                         , profileHomeTel=htel
-                         , profileDesiredCourse=dc
-                         , profileDesiredWorkLocation=dwl
-                         , profileEmployment=emp
-                         }
-      Just (pid, _) -> do
-        update pid [ ProfileBirth bir
-                   , ProfileEntryYear ey
-                   , ProfileGraduateYear gy
-                   , ProfileBranch br
-                   , ProfileZip zip
-                   , ProfileAddress adr
-                   , ProfileLongitude lon'
-                   , ProfileLatitude lat'
-                   , ProfileTel tel
-                   , ProfileStation st
-                   , ProfileHomeZip hzip
-                   , ProfileHomeAddress hadr
-                   , ProfileHomeLongitude hlon'
-                   , ProfileHomeLatitude hlat'
-                   , ProfileHomeTel htel
-                   , ProfileDesiredCourse dc
-                   , ProfileDesiredWorkLocation dwl
-                   , ProfileEmployment emp
-                   ]
-        return pid
-  redirectParams RedirectTemporary (ProfileR uid) [("mode", "e")]
+  user <- runDB $ get404 uid
+  let editable = selfid == uid || userRole self > userRole user
+  unless editable $ 
+    permissionDenied "あなたはこのユーザプロファイルを編集することはできません."
+  case userRole user of
+    Student -> putStudentProf user
+    _       -> putTeacher user
+  where
+    putTeacher user = do
+      (em, fn, gn) <- 
+        runFormPost' $ (,,)
+        <$> emailInput "email"
+        <*> stringInput "familyName"
+        <*> stringInput "givenName"
+      runDB $ do
+        -- update user
+        update uid [UserEmail em, UserFamilyName fn, UserGivenName gn]
+      redirectParams RedirectTemporary (ProfileR uid) [("mode", "e")]
+      
+    putStudentProf user = do
+      (em, fn, gn) <- 
+        runFormPost' $ (,,)
+        <$> emailInput "email"
+        <*> stringInput "familyName"
+        <*> stringInput "givenName"
+      (bir, ey, gy, br, zip, adr, lon, lat, tel, st, hzip, hadr, hlon, hlat, htel, dc, dwl, emp) <- 
+        runFormPost' $ (,,,,,,,,,,,,,,,,,)
+        <$> dayInput "birth"
+        <*> intInput "entryYear"
+        <*> maybeIntInput "graduateYear"
+        <*> stringInput "branch"
+        <*> stringInput "zip"
+        <*> stringInput "address"
+        <*> maybeStringInput "longitude"
+        <*> maybeStringInput "latitude"
+        <*> stringInput "tel"
+        <*> stringInput "station"
+        <*> stringInput "homeZip"
+        <*> stringInput "homeAddress"
+        <*> maybeStringInput "homeLongitude"
+        <*> maybeStringInput "homeLatitude"
+        <*> stringInput "homeTel"
+        <*> maybeStringInput "desiredCourse"
+        <*> maybeStringInput "desiredWorkLocation"
+        <*> maybeStringInput "employment"
+      let lon' = fromMaybe Nothing (fmap (Just . read) lon)
+          lat' = fromMaybe Nothing (fmap (Just . read) lat)
+          hlon' = fromMaybe Nothing (fmap (Just . read) hlon)
+          hlat' = fromMaybe Nothing (fmap (Just . read) hlat)
+      runDB $ do
+        -- update user
+        update uid [UserEmail em, UserFamilyName fn, UserGivenName gn]
+        mprof <- getBy $ UniqueProfile uid
+        case mprof of
+          Nothing -> do
+            insert $ Profile { profileUser=uid
+                             , profileBirth=bir
+                             , profileEntryYear=ey
+                             , profileGraduateYear=gy
+                             , profileBranch=br
+                             , profileZip=zip
+                             , profileAddress=adr
+                             , profileLongitude=lon'
+                             , profileLatitude=lat'
+                             , profileTel=tel
+                             , profileStation=st
+                             , profileHomeZip=hzip
+                             , profileHomeAddress=hadr
+                             , profileHomeLongitude=hlon'
+                             , profileHomeLatitude=hlat'
+                             , profileHomeTel=htel
+                             , profileDesiredCourse=dc
+                             , profileDesiredWorkLocation=dwl
+                             , profileEmployment=emp
+                             }
+          Just (pid, _) -> do
+            update pid [ ProfileBirth bir
+                       , ProfileEntryYear ey
+                       , ProfileGraduateYear gy
+                       , ProfileBranch br
+                       , ProfileZip zip
+                       , ProfileAddress adr
+                       , ProfileLongitude lon'
+                       , ProfileLatitude lat'
+                       , ProfileTel tel
+                       , ProfileStation st
+                       , ProfileHomeZip hzip
+                       , ProfileHomeAddress hadr
+                       , ProfileHomeLongitude hlon'
+                       , ProfileHomeLatitude hlat'
+                       , ProfileHomeTel htel
+                       , ProfileDesiredCourse dc
+                       , ProfileDesiredWorkLocation dwl
+                       , ProfileEmployment emp
+                       ]
+            return pid
+      redirectParams RedirectTemporary (ProfileR uid) [("mode", "e")]
     
 getAvatarImageR :: UserId -> Handler RepHtml
 getAvatarImageR uid = do

bdf4126ee7213dc2707f6f52381481eaae16cf88
cutsea110 <cutsea110@gmail.com>
2012-11-09 00:23:22 +0900

    Add jquery plugin zClip, which can clipboard copy at clicked

--- Foundation.hs
+++ Foundation.hs
@@ -119,6 +119,7 @@ instance Yesod BISocie where
         addScriptEither $ Left $ StaticR plugins_ajaxzip2_ajaxzip2_js
         addScriptEither $ Left $ StaticR plugins_selection_jquery_selection_min_js
         addScriptEither $ Left $ StaticR plugins_textchange_jquery_textchange_min_js
+        addScriptEither $ Left $ StaticR plugins_zClip_jquery_zclip_min_js
         toWidget $(cassiusFile "templates/default-layout.cassius")
         toWidget $(juliusFile "templates/default-layout.julius")
       hamletToRepHtml $(hamletFile "templates/default-layout.hamlet")

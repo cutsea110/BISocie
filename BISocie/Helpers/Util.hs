2c5d563f542bb7e4481522100ab38c2a064e75de
cutsea110 <cutsea110@gmail.com>
2012-12-21 21:52:25 +0900

    export csv for google calendar
    Implement ExportCsvR

--- BISocie/Helpers/Util.hs
+++ BISocie/Helpers/Util.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module BISocie.Helpers.Util
        ( mkPagenate
        , (+++)
@@ -10,15 +11,22 @@ module BISocie.Helpers.Util
        , snd3
        , thd3
        , ilike
+         -- for CSV
+       , RepCsv(..)
+       , CSV(..)
+       , ToText(..)
+       , download
        ) where
 
-import Yesod
-import Database.Persist.Store
 import Codec.Binary.UTF8.String (encodeString, decodeString)
+import Control.Arrow ((***))
+import Database.Persist.Store
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Time
 import Data.Tuple.HT (fst3, snd3, thd3)
 import Network.HTTP.Base (urlEncode, urlDecode)
+import Yesod
 
 (+++) :: Text -> Text -> Text
 (+++) = T.append
@@ -58,3 +66,42 @@ encodeUrl = T.pack . urlEncode . encodeString . T.unpack
 
 decodeUrl :: T.Text -> T.Text
 decodeUrl = T.pack . decodeString . urlDecode . T.unpack
+
+
+newtype CSV a = CSV { unCsv :: ([Text],[[a]]) } deriving Show
+
+instance ToText a => ToContent (CSV a) where
+  toContent = toContent.trans
+    where
+      trans :: ToText a => CSV a -> Text
+      trans = T.unlines.uncurry (:).(xHead *** xBody).unCsv
+      xBody :: ToText a => [[a]] -> [Text]
+      xBody = map (T.intercalate "," . map toText)
+      xHead  :: [Text] -> Text
+      xHead = T.intercalate ","
+
+class ToText a where
+  toText :: a -> Text
+
+instance ToText Text where
+  toText = id
+instance ToText Int where
+  toText = T.pack . show
+instance ToText Double where
+  toText = T.pack . show
+instance ToText Integer where
+  toText = T.pack . show
+instance ToText Day where
+  toText = T.pack . show
+instance ToText TimeOfDay where
+  toText = T.pack . show
+
+newtype RepCsv a = RepCsv (CSV a)
+
+instance ToText a => HasReps (RepCsv a) where
+  chooseRep (RepCsv csv) _ = return (typeOctet, toContent csv)
+
+download :: ToText a => Text -> CSV a -> GHandler s m (RepCsv a)
+download fn csv = do
+  setHeader "Content-Disposition" $ "attachment; filename=" `T.append` fn
+  return (RepCsv csv)

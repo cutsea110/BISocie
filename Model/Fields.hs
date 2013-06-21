941000beacb1e72e10569cee163fbcb6a2ea1fe2
cutsea110 <cutsea110@gmail.com>
2013-06-21 18:56:42 +0900

    Export Model.Fields

---
+++ Model/Fields.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Model.Fields where
+
+import Prelude
+import Yesod
+
+data Role =  Student | Teacher | Staff | Admin
+          deriving (Read, Show, Eq, Ord, Enum, Bounded)
+derivePersistField "Role"

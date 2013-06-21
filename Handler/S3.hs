05ce080694833119b321e36086555bdfbb977351
cutsea110 <cutsea110@gmail.com>
2013-06-21 15:42:02 +0900

    Catch up yesod-platform-1.2
    migrate Handler/S3.hs only

--- Handler/S3.hs
+++ Handler/S3.hs
@@ -18,18 +18,20 @@ import qualified Data.ByteString.Lazy as L
 import Data.Conduit (($$))
 import Data.Conduit.List (consume)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import Data.Time
 import System.Directory
 import System.FilePath
+import Text.Hamlet (xshamlet)
 
-getUploadR :: Handler RepHtml
+getUploadR :: Handler Html
 getUploadR = do
   (Entity uid _) <- requireAuth
   defaultLayout $ do
     $(widgetFile "s3/upload")
 
 upload uid fi = do
-  lbs <- lift $ lift $ fileContent fi
+  lbs <- lift $ fileContent fi
   let fsize = L.length lbs
   if fileName' fi /= "" && L.length lbs > 0
     then do
@@ -57,7 +59,7 @@ upload uid fi = do
     fileName' :: FileInfo -> T.Text
     fileName' = last . T.split (\c -> c=='/' || c=='\\') . fileName
     fileContent f = L.fromChunks <$> (fileSource f $$ consume)
-  
+
 postUploadR :: Handler RepXml
 postUploadR = do
   (Entity uid _) <- requireAuth
@@ -72,8 +74,8 @@ postUploadR = do
         Just (fid, name, ext, fsize, cdate) -> do
           cacheSeconds 10 -- FIXME
           let rf = r $ FileR uid fid
-          fmap RepXml $ hamletToContent
-                      [xhamlet|$newline never
+          return $ RepXml $ toContent
+            [xshamlet|$newline never
 <file>
   <fhid>#{T.unpack $ toPathPiece fid}
   <name>#{name}
@@ -83,7 +85,7 @@ postUploadR = do
   <uri>#{rf}
 |]
 
-putUploadR :: Handler RepHtml
+putUploadR :: Handler Html
 putUploadR = do
   (Entity uid _) <- requireAuth
   mfi <- lookupFile "upfile"
@@ -96,14 +98,14 @@ putUploadR = do
         Just (fid, _, _, _, _) -> sendResponseCreated $ FileR uid fid
 
 
-getFileR :: UserId -> FileHeaderId -> Handler RepHtml
+getFileR :: UserId -> FileHeaderId -> Handler ()
 getFileR uid fid = do
   h <- runDB $ get404 fid
   let s3dir' = s3dir </> T.unpack (toPathPiece uid)
       s3fp = s3dir' </> T.unpack (toPathPiece fid)
-  setHeader "Content-Type" $ fileHeaderContentType h
-  setHeader "Content-Disposition" $ "attachment; filename=" +++ fileHeaderEfname h
-  return $ RepHtml $ ContentFile s3fp Nothing
+  addHeader "Content-Type" $ fileHeaderContentType h
+  addHeader "Content-Disposition" $ "attachment; filename=" +++ fileHeaderEfname h
+  sendFile (TE.encodeUtf8 (fileHeaderContentType h)) s3fp
 
 postFileR :: UserId -> FileHeaderId -> Handler RepXml
 postFileR uid fid = do
@@ -126,19 +128,19 @@ deleteFileR uid fid = do
         s3fp = s3dir' </> T.unpack (toPathPiece fid)
         rf = r $ FileR uid fid
     liftIO $ removeFile s3fp
-    fmap RepXml $ hamletToContent
-                  [xhamlet|$newline never
+    return $ RepXml $ toContent
+      [xshamlet|$newline never
 <deleted>
   <uri>#{rf}
 |]
 
-getFileListR :: UserId -> Handler RepJson
+getFileListR :: UserId -> Handler Value
 getFileListR uid = do
   _ <- requireAuth
   render <- getUrlRender
   files <- runDB $ selectList [FileHeaderCreator ==. uid] [Desc FileHeaderCreated]
   cacheSeconds 10 -- FIXME
-  jsonToRepJson $ object ["files" .= array (map (go render) files)]
+  returnJson $ object ["files" .= array (map (go render) files)]
   where
     go r (Entity fid FileHeader
                { fileHeaderFullname = name
